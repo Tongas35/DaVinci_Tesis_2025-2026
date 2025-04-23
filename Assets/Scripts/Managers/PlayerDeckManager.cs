105f5a2ee7a76f443bb68498ab5f4f01818daf94
Gastón Sanmiguel <sanmiguelgaston35@gmail.com>
2025-04-23 16:27:50 -0300

    notas adicionales para algunas cosas

--- Assets/Scripts/Managers/PlayerDeckManager.cs
+++ Assets/Scripts/Managers/PlayerDeckManager.cs
@@ -15,19 +15,27 @@ public class PlayerDeckManager : MonoBehaviour
 
     [Header("Configuración de Mazo")]
     public List<Card> fullDeck;
+    
+    // Cantidad de cartas que roba el player cuando arranca el dia
     public int startingHandSize = 5;
+    // Cantidad de cartas maxima en la mano
     public int maxHandSize = 8;
 
+    // Lista del Mazo
     private List<Card> drawPile;
+    // Lista del Mazo de descarte
     private List<Card> discardPile;
+    // Mano
     public List<Card> hand { get; private set; }
 
     private void Awake()
     {
+        // Setteo de mano
         if (Instance != null && Instance != this) Destroy(gameObject);
         else Instance = this;
     }
 
+    // Setteo de Mazo y Draw inicial
     public void InitializeDeck()
     {
         drawPile = new List<Card>(fullDeck);
@@ -38,6 +46,12 @@ public class PlayerDeckManager : MonoBehaviour
             DrawCard();
     }
 
+    /// <summary>
+    /// Metodo para toamr una carta del Mazo.
+    /// Ignora si la mano esta llena.
+    /// En caso de no tener mas cartas en el mazo principal, toma el mazo de descarte lo mezcla y suma al deck principal.
+    /// En ultimo caso, toma una carta del mazo y la agrega a la mano.
+    /// </summary>
     public void DrawCard()
     {
         if (hand.Count >= maxHandSize) return;
@@ -54,12 +68,16 @@ public class PlayerDeckManager : MonoBehaviour
         HandManager.Instance.MoveToNextSlot(c);
     }
 
+    // Retira cartas de la mano
     public void DiscardCard(Card c)
     {
         if (!hand.Remove(c)) return;
         discardPile.Add(c);
     }
 
+    /// <summary>
+    /// Funcion de mezclado automatico de listas. Usado para los Mazos.
+    /// </summary> 
     private void Shuffle<T>(List<T> list)
     {
         for (int i = 0; i < list.Count; i++)
